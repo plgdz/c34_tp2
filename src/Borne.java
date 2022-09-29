ec325d408f929300a20f9ffa388a3148777e957e
plgdz <40022026+plgdz@users.noreply.github.com>
2022-09-28 20:34:40 -0400

    JDE 12.0.2 + attribut transaction et borne

--- src/Borne.java
+++ src/Borne.java
@@ -1,2 +1,10 @@
 public class Borne {
+    public static final int prixHeure = 3;
+    private static final String regIdStationnement = "[A-D]{1}\\d{3}";
+    private static final double tempsMax = 2;
+    private double montantTransaction;
+    private double sommeTotale;
+    private Transaction transaction;
+
+
 }
--- src/Transaction.java
+++ src/Transaction.java
@@ -1,2 +1,9 @@
 public class Transaction {
+    private String numTransaction;
+    private int dureeStationnement;
+    private double montant;
+    private int typePaiement;
+    private CarteCredit carte;
+
+
 }
